7d0b1c318ae29e7429604254af4fc0e67d2e627c
sazukeR <reinaldojcg1@gmail.com>
2023-11-12 17:16:14 -0500

    Add linters config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,4 @@
-module.exports = {
+/* module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
@@ -10,6 +10,26 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
+ */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
